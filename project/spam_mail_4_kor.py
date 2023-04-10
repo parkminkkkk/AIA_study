001f427b303bd1de9a6264860311d95b2c3c47e9
parkminkkkk <cyzlalsrud@naver.com>
2023-04-11 00:37:57 +0900

    aia

--- project/spam_mail_4_kor.py
+++ project/spam_mail_4_kor.py
@@ -5,8 +5,10 @@ import seaborn as sns
 #
 import nltk
 # nltk.download('punkt')
-from konlpy.tag import Okt
+# from konlpy.tag import Okt
+# from transformers import AutoTokenizer
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
@@ -40,6 +42,7 @@ print(type(dt_eng)) #<class 'pandas.core.frame.DataFrame'>
 # dt_eng = dt_eng.values 
 # print(type(dt_eng)) #<class 'numpy.ndarray'>
 
+print(dt_kor) #[99 rows x 3 columns]
 
 #Eng_Text processing 
 #Remove stopwords from the data
@@ -50,11 +53,16 @@ dt_eng['text'] = dt_eng['text'].apply(lambda x: ' '.join([ word for word in word
 # print(dt_eng['text'][0])
 
 #Kor_Text processing 
-tokenizer = Okt()
-data['text'] = data['text'].apply(lambda x: ' '.join(tokenizer.morphs(x)))
-tfidf = TfidfVectorizer(stop_words=['은', '는', '이', '가', '을', '를'])
-X = tfidf.fit_transform(data['text'])
-y = data['class']
+# tokenizer = Okt()
+# data['text'] = data['text'].apply(lambda x: ' '.join(tokenizer.morphs(x)))
+# tfidf = TfidfVectorizer(stop_words=['은', '는', '이', '가', '을', '를'])
+# X = tfidf.fit_transform(data['text'])
+# y = data['class']
+
+# 먼저 train 데이터와 test 데이터 인덱스 없이 배열로 만들기
+kor_x = np.array([x for x in dt_kor['text']])
+print(kor_x)
+kor_y = dt_kor.loc[:,'class']
 
 eng_x = dt_eng.loc[:,'text'] 
 eng_y = dt_eng.loc[:,'class']
@@ -63,10 +71,22 @@ print(type(eng_x)) #<class 'pandas.core.series.Series'>
 x_train, x_test, y_train, y_test = train_test_split (eng_x, eng_y, train_size=0.7, random_state=42)
 print(x_train.shape, x_test.shape) #(3619,) (1552,)
 
+kx_train, kx_test, ky_train, ky_test = train_test_split (kor_x, kor_y, train_size=0.7, random_state=42)
+
+
 #vectorizer
 # cVect = CountVectorizer()
 tfVect = TfidfVectorizer()
-#두개 비교
+
+#Tokenizer
+vocab_size = 2000 
+tokenizer = Tokenizer(num_words = vocab_size)  
+tokenizer.fit_on_texts(kx_train) 
+sequences_train = tokenizer.texts_to_sequences(kx_train) 
+sequences_test = tokenizer.texts_to_sequences(kx_test)  
+print(len(sequences_train), len(sequences_test)) #69 30
+
+
 
 tfVect.fit(x_train)
 train_engV = tfVect.transform(x_train).toarray()
@@ -74,17 +94,66 @@ test_engV = tfVect.transform(x_test).toarray()
 print(train_engV.shape[0], test_engV.shape[0]) #3619 #1552
 print(train_engV.shape[1], test_engV.shape[1]) #41290 # 41290
 
-#model
-lr = LogisticRegression(verbose=1)
-#모델 다른거쓰기 
+
+# 변환된 시퀀스 번호를 이용해 단어 임베딩 벡터 생성
+word_index = tokenizer.word_index
+max_length = 14
+padding_type='post'
+train_korx = pad_sequences(sequences_train, padding='post', maxlen=max_length)
+test_korx = pad_sequences(sequences_test, padding=padding_type, maxlen=max_length)
+
+print(train_korx.shape, test_korx.shape) #(69, 14) (30, 14)
+print(train_korx)
+
+train_engV = pad_sequences(train_engV, padding='post', maxlen=max_length)
+test_engV = pad_sequences(test_engV, padding=padding_type, maxlen=max_length)
+print(train_engV)
+
+train_korx= train_korx.reshape(-1,max_length,1)
+test_korx= test_korx.reshape(-1,max_length,1)
+train_engV= train_engV.reshape(-1,max_length,1)
+test_engV= test_engV.reshape(-1,max_length,1)
+
+
+
+#model1
+input1 = Input(shape=(14,1))
+dense1 = LSTM(16, activation='relu', name='kor1')(input1)
+dense2 = Dense(16, activation='relu', name='kor2')(dense1)
+dense4 = Dense(16, activation='swish', name='kor4')(dense2)
+output1 = Dense(16, name='output2')(dense4)
+#model2
+input2 = Input(shape=(14,1))
+dense11 = LSTM(16, activation='relu', name='eng1')(input2)
+dense12 = Dense(16, activation='relu', name='eng2')(dense11)
+dense14 = Dense(16, activation='swish', name='eng4')(dense12)
+output2 = Dense(16, name='output2')(dense14)
+
+#2-3. 모델 합침 
+merge1 = concatenate([output1, output2], name='mg1')  
+merge2 = Dense(32, activation='selu', name='mg2')(merge1)
+merge3 = Dense(32, activation='swish', name='mg3')(merge2)
+merge4 = Dense(16, activation='relu', name='mg4')(merge3)
+last_output = Dense(1,name='last')(merge3)
+
+#2-4 모델 정의 
+model = Model(inputs=[input1, input2], outputs=[last_output])
+
+# model.summary()
 
 #fit
-lr.fit(train_engV, y_train)
+model. compile(loss='loss', optimizer='adam')
+
+model.fit(train_engV, y_train)
+model.fit([train_korx, train_engV], y_train, epochs=1, batch_size=128, validation_split=0.2,
+          )
 
 #Predic, Evaluate
-pred = lr.predict(test_engV)
+acc = model.evaluate([test_korx, test_engV], y_test)
 
-print('Accuracy: ', accuracy_score(y_test, pred))
+# pred = model.predict([test_korx, test_engV])
+
+print('Accuracy: ', acc)
 
 '''
 Accuracy:  0.9787371134020618
