26e6d16c6553acaf652898076de58211da413ce3
parkminkkkk <cyzlalsrud@naver.com>
2023-04-18 13:10:04 +0900

    aia

---
+++ ml/m11_GridSearch08_RF_diabets.py
@@ -0,0 +1,70 @@
+#실습 
+#모델 : RandomForestClassifier
+# parameters = [
+#     {'n_estimators' : [100,200]},
+#     {'max_depth' : [6,8,10,12]},
+#     {'min_samples_leaf' : [3,5,7,10]},
+#     {'min_samples_split' : [2,3,5,10]},
+#     {'n_jobs' : [-1, 2, 4]}]
+#파라미터 조합으로 2개 이상 엮을 것
+####################################################
+import time
+import numpy as np
+from sklearn.datasets import load_iris, load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, cross_val_score, StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.model_selection import GridSearchCV   
+from sklearn.metrics import accuracy_score, r2_score
+
+#1. 데이터 
+x, y = load_diabetes(return_X_y=True)
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, shuffle=True, random_state=42, test_size=0.2
+)
+
+n_splits = 5
+kfold = KFold(n_splits=n_splits, shuffle=True, random_state=337)
+# kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=337)
+
+
+parameters = [
+    {'n_estimators' : [100,200]}, {'max_depth' : [6,8,10,12]}, {'min_samples_leaf' : [3,5,7,10]},
+    {'max_depth' : [6,8,10,12]}, {'min_samples_leaf' : [3,5,7,10]},
+    {'min_samples_leaf' : [3,5,7,10]}, {'min_samples_split' : [2,3,5,10]},
+    {'min_samples_split' : [2,3,5,10]},
+  ]
+
+#2. 모델 
+model = GridSearchCV(RandomForestRegressor(), parameters,
+                     cv=kfold, verbose=1, refit=True, n_jobs=-1)
+
+#3. 컴파일, 훈련 
+start_time = time.time()
+model.fit(x_train, y_train)
+end_time = time.time()
+
+print("최적의 매개변수:", model.best_estimator_) 
+print("최적의 파라미터:", model.best_params_)
+print("best_score:", model.best_score_)
+print("model.score:", model.score(x_test, y_test))
+print("걸린시간 :", round(end_time-start_time,2), "초")
+
+#4. 평가, 예측
+y_predict = model.predict(x_test)
+print("r2_score:", r2_score(y_test, y_predict))
+
+y_pred_best = model.best_estimator_.predict(x_test)            
+print("최적 튠 r2:", r2_score(y_test, y_pred_best))
+
+'''
+Fitting 5 folds for each of 30 candidates, totalling 150 fits
+최적의 매개변수: RandomForestRegressor(min_samples_leaf=7)
+최적의 파라미터: {'min_samples_leaf': 7}
+best_score: 0.4476214576923686
+model.score: 0.48974147346686603
+걸린시간 : 9.87 초
+r2_score: 0.48974147346686603
+최적 튠 r2: 0.48974147346686603
+'''
