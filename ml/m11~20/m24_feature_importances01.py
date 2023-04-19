b5ba8b61bd5541d907b94db64e6d96c8fa6f2dbe
parkminkkkk <cyzlalsrud@naver.com>
2023-04-19 15:37:38 +0900

    aia

---
+++ ml/m11~20/m24_feature_importances01.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
+
+#1. 데이터 
+x,y = load_iris(return_X_y=True)
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size=0.8, shuffle=True, random_state=337
+)
+
+#2. 모델구성
+# scaler +모델 합침
+# model = make_pipeline(MinMaxScaler(), RandomForestClassifier())
+# model = make_pipeline(StandardScaler(), RandomForestClassifier())
+model = make_pipeline(StandardScaler(), SVC())
+
+
+#3. 훈련 
+model.fit(x_train, y_train)
+
+#4. 평가, 예측
+result = model.score(x_test, y_test)
+print("model.score:", result)
+
+y_predict = model.predict(x_test)
+acc = accuracy_score(y_test, y_predict)
+print("accuracy_score:", acc)
+
+'''
+#model = make_pipeline(StandardScaler(), SVC())
+model.score: 0.9333333333333333
+accuracy_score: 0.9333333333333333
+'''
+
